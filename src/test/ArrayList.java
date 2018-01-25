a35dc3ecb951a45f7b732db38a02b65512893850
Sachin Araballi <sachinaraballi@gmial.com>
2018-01-25 19:48:47 +0530

    ArrayList implementation in Java
    Change-Id: Ibc5366df40c1ebb3d85eb509d68e193882547a86
    Signed-off-by: Sachin Araballi <sachinaraballi@gmial.com>

---
+++ src/test/ArrayList.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.util.Arrays;
+
+class MyList<E> {
+
+    private int size = 0;
+    private static final int THRESHOLD_CAPACITY = 4;
+    private Object[] elements;
+
+    MyList() {
+        elements = new Object[THRESHOLD_CAPACITY];
+    }
+
+    public void add(E e) {
+        if (size == elements.length) {
+            ensureCapacity();
+        }
+        elements[size++] = e;
+    }
+
+    private void ensureCapacity() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    @SuppressWarnings("unchecked")
+    public E get(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        return (E) elements[i];
+    }
+
+    public void remove(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        for (int x = i; x < elements.length - 1; x++) {
+            elements[x] = elements[x + 1];
+        }
+        size--;
+    }
+    
+    public int size(){
+        return size;
+    }
+}
+
+public class ArrayList {
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.remove(0);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
